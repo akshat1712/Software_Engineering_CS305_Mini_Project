659767cfe2aedab72e92d38c5d089c8427c56407
Akshat Toolaj Sinha <2020csb1068@iitrpr.ac.in>
2023-02-11 15:41:43 +0530

    DATBASE FILES ADDED

---
+++ MODELS/FUNCTIONS.sql
@@ -0,0 +1,216 @@
+
+CREATE OR REPLACE FUNCTION INSERT_DEPARTMENT(
+    dept_name VARCHAR(50)
+)
+RETURNS INTEGER
+LANGUAGE plpgsql
+AS $$
+    BEGIN
+        INSERT INTO departments ("name")
+        VALUES (dept_name);
+        RETURN 1;
+    END;
+$$;
+
+
+CREATE OR REPLACE FUNCTION INSERT_STUDENT(
+    student_name VARCHAR(50),
+    entry_number VARCHAR(15),
+    email VARCHAR(50),
+    dept_id INTEGER,
+    batch INTEGER,
+    phone_number VARCHAR(10),
+    address VARCHAR(100)
+)
+RETURNS INTEGER
+LANGUAGE plpgsql
+AS $$
+    BEGIN
+        INSERT INTO students ("name", "entry_number", "email", "dept_id", "batch", "phone_number", "address")
+        VALUES (student_name, entry_number, email, dept_id, batch, phone_number, address);
+        RETURN 1;
+    END;
+$$;
+
+CREATE OR REPLACE FUNCTION INSERT_FACULTY(
+    faculty_name VARCHAR(50),
+    email VARCHAR(50),
+    dept_id INTEGER,
+    joining_date DATE,
+    phone_number VARCHAR(10),
+    address VARCHAR(100)
+)
+RETURNS INTEGER
+LANGUAGE plpgsql
+AS $$
+    BEGIN
+        INSERT INTO faculties ("name", "email", "dept_id", "joining_date", "phone_number", "address")
+        VALUES (faculty_name, email, dept_id, joining_date, phone_number, address);
+        RETURN 1;
+    END;
+$$;
+
+
+CREATE OR REPLACE FUNCTION INSERT_ACADEMIC_EMPLOYEE(
+    name VARCHAR(50),
+    email VARCHAR(50),
+    joining_date DATE,
+    phone_number VARCHAR(10),
+    address VARCHAR(100)
+)
+RETURNS INTEGER
+LANGUAGE plpgsql
+AS $$
+    BEGIN
+        INSERT INTO acad_empl("name", "email", "joining_date", "phone_number", "address")
+        VALUES (name, email,joining_date, phone_number, address);
+        RETURN 1;
+    END;
+$$;
+
+
+CREATE OR REPLACE FUNCTION INSERT_LOGIN_LOG(
+    email VARCHAR(50),
+    login_time TIMESTAMP,
+    logout_time TIMESTAMP
+)
+RETURNS INTEGER
+LANGUAGE plpgsql
+AS $$
+    BEGIN
+        INSERT INTO login_logs ("email", "login_time", "logout_time")
+        VALUES (email, login_time, logout_time);
+        RETURN 1;
+    END;
+$$;
+
+CREATE OR REPLACE FUNCTION INSERT_PASSWORD(
+    email VARCHAR(50),
+    password VARCHAR(50),
+    role VARCHAR(10)
+)
+RETURNS INTEGER
+LANGUAGE plpgsql
+AS $$
+    BEGIN
+        INSERT INTO passwords ("email", "password", "role")
+        VALUES (email, password, role);
+        RETURN 1;
+    END;
+$$;
+
+
+CREATE OR REPLACE FUNCTION INSERT_COURSE_CATALOG(
+    course_name VARCHAR(50),
+    course_code VARCHAR(10),
+    dept_id INTEGER,
+    pre_req TEXT[],
+    lectures INTEGER,
+    tutorials INTEGER,
+    practicals INTEGER,
+    self_study INTEGER,
+    credits INTEGER
+)
+RETURNS INTEGER
+LANGUAGE plpgsql
+AS $$
+    BEGIN
+        INSERT INTO courses_catalog ("course_name", "course_code", "dept_id", "pre_req", "lectures", "tutorials", "practicals", "self_study", "credits")
+        VALUES (course_name, course_code, dept_id, pre_req, lectures, tutorials, practicals, self_study, credits);
+        RETURN 1;
+    END;
+$$;
+
+
+CREATE OR REPLACE FUNCTION INSERT_COURSE_OFFERED(
+    catalog_id INTEGER,
+    faculty_id INTEGER,
+    course_code VARCHAR(5),
+    CGPA_cutoff REAL,
+    courses_pre_req TEXT[]
+)
+RETURNS INTEGER
+LANGUAGE plpgsql
+AS $$
+    BEGIN
+        INSERT INTO courses_offered ("catalog_id", "faculty_id", "course_code", "CGPA_cutoff", "courses_pre_req")
+        VALUES (catalog_id, faculty_id, course_code, CGPA_cutoff, courses_pre_req);
+        RETURN 1;
+    END;
+$$;
+
+
+
+
+
+CREATE OR REPLACE FUNCTION STUDENT_TABLE_CREATION()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS
+$$
+    DECLARE
+    BEGIN
+            EXECUTE '
+                CREATE TABLE IF NOT EXISTS transcript_student_'||new.student_id||' (
+                "catalog_id" INTEGER NOT NULL,
+                "grade" VARCHAR(2) NOT NULL,
+                "semester" VARCHAR(1) NOT NULL,
+                "year" VARCHAR(4) NOT NULL,
+                FOREIGN KEY ("catalog_id") REFERENCES "courses_catalog" ("catalog_id"),
+                PRIMARY KEY ( "catalog_id", "semester", "year")
+                );
+            ';
+
+            EXECUTE '
+                CREATE TABLE IF NOT EXISTS courses_enrolled_student_'||new.student_id||' (
+                "catalog_id" INTEGER NOT NULL,
+                "grade" VARCHAR(2) ,
+                FOREIGN KEY ("catalog_id") REFERENCES "courses_catalog" ("catalog_id"),
+                PRIMARY KEY ( "catalog_id")
+                );
+            ';
+        RETURN NEW;
+    END
+$$;
+
+
+CREATE OR REPLACE TRIGGER CREATE_STUDENT_RECORDS_TABLES
+AFTER INSERT ON students
+FOR EACH ROW EXECUTE PROCEDURE STUDENT_TABLE_CREATION();
+
+
+
+CREATE OR REPLACE FUNCTION FACULTY_TABLE_CREATION()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS
+$$
+    DECLARE
+    BEGIN
+            EXECUTE '
+                CREATE TABLE IF NOT EXISTS transcript_faculty_'||new.faculty_id||' (
+                "catalog_id" INTEGER NOT NULL,
+                "semester" VARCHAR(1) NOT NULL,
+                "year" VARCHAR(4) NOT NULL,
+                FOREIGN KEY ("catalog_id") REFERENCES "courses_catalog" ("catalog_id"),
+                PRIMARY KEY ( "catalog_id", "semester", "year")
+                );
+            ';
+
+            EXECUTE '
+                CREATE TABLE IF NOT EXISTS courses_teaching_faculty_'||new.faculty_id||' (
+                "catalog_id" INTEGER NOT NULL,
+                FOREIGN KEY ("catalog_id") REFERENCES "courses_catalog" ("catalog_id"),
+                PRIMARY KEY ( "catalog_id")
+                );
+            ';
+        RETURN NEW;
+    END
+$$;
+
+
+CREATE OR REPLACE TRIGGER CREATE_FACULTY_RECORDS_TABLES
+AFTER INSERT ON faculties
+FOR EACH ROW EXECUTE PROCEDURE FACULTY_TABLE_CREATION();
+
+
---
+++ MODELS/TABLES.sql
@@ -0,0 +1,102 @@
+CREATE TABLE IF NOT EXISTS "departments" (
+    "dept_id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS "students" (
+    "student_id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(50) NOT NULL,
+    "entry_number" VARCHAR(15) NOT NULL UNIQUE,
+    "email" VARCHAR(50) NOT NULL UNIQUE,
+    "dept_id" INTEGER NOT NULL,
+    "batch" INTEGER NOT NULL,
+    "phone_number" VARCHAR(10) NOT NULL UNIQUE,
+    "address" VARCHAR(100) NOT NULL,
+    FOREIGN KEY ("dept_id") REFERENCES "departments" ("dept_id")
+);
+
+CREATE TABLE IF NOT EXISTS "faculties" (
+    "faculty_id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(50) NOT NULL UNIQUE,
+    "dept_id" INTEGER NOT NULL,
+    "joining_date" DATE NOT NULL,
+    "phone_number" VARCHAR(10) NOT NULL UNIQUE,
+    "address" VARCHAR(100) NOT NULL,
+    FOREIGN KEY ("dept_id") REFERENCES "departments" ("dept_id")
+);
+
+CREATE TABLE IF NOT EXISTS "acad_empl" (
+    "empl_id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(50) NOT NULL UNIQUE,
+    "joining_date" DATE NOT NULL,
+    "phone_number" VARCHAR(10) NOT NULL UNIQUE,
+    "address" VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "login_logs" (
+    "log_id" SERIAL PRIMARY KEY,
+    "email" VARCHAR(50) NOT NULL,
+    "login_time" TIMESTAMP NOT NULL,
+    "logout_time" TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "passwords" (
+    "email" VARCHAR(50) NOT NULL,
+    "password" VARCHAR(50) NOT NULL,
+    "role" VARCHAR(10) NOT NULL,
+    PRIMARY KEY ("email")
+);
+
+--  In pre_req, it will be course followed by grade
+
+CREATE TABLE IF NOT EXISTS "courses_catalog" (
+    "catalog_id" SERIAL PRIMARY KEY,
+    "course_code" VARCHAR(5) NOT NULL,
+    "course_name" VARCHAR(50) NOT NULL,
+    "dept_id" INTEGER NOT NULL,
+    "pre_req" TEXT[],
+    "lectures" VARCHAR(1) NOT NULL,
+    "tutorials" VARCHAR(1) NOT NULL,
+    "practicals" VARCHAR(1) NOT NULL,
+    "self_study" VARCHAR(1) NOT NULL,
+    "credits" VARCHAR(1) NOT NULL,
+    FOREIGN KEY ("dept_id") REFERENCES "departments" ("dept_id")
+);
+
+CREATE TABLE IF NOT EXISTS "courses_offering" (
+    "offering_id" SERIAL PRIMARY KEY,
+    "catalog_id" INTEGER,
+    "faculty_id" INTEGER NOT NULL,
+    "course_code" VARCHAR(5) NOT NULL UNIQUE,
+    "offering_pre_req" TEXT[],
+    "CGPA" VARCHAR(1) NOT NULL,
+    FOREIGN KEY ("catalog_id") REFERENCES "courses_catalog" ("catalog_id"),
+    FOREIGN KEY ("faculty_id") REFERENCES "faculties" ("faculty_id")
+);
+
+CREATE TABLE IF NOT EXISTS "capstone" (
+    "student_id" INTEGER NOT NULL,
+    "faculty_id" INTEGER NOT NULL,
+    FOREIGN KEY ("student_id") REFERENCES "students" ("student_id"),
+    FOREIGN KEY ("faculty_id") REFERENCES "faculties" ("faculty_id"),
+    PRIMARY KEY ("student_id")
+);
+
+CREATE TABLE IF NOT EXISTS "time_semester"(
+    "semester" VARCHAR(1) NOT NULL,
+    "year" INTEGER NOT NULL,
+    PRIMARY KEY ("semester", "year")
+);
+
+
+-- FOR EACH BATCH, I have to make a table for each batch which tells the credits requirement for passing the
+-- engineering and
+
+-- THIS IS FOR ONLY COURSES, THERE IS DIFFERENT TABLE FOR CREDITS
+-- department,program_core,humanties_core,science_core,general_engineering,capstone,internship,extra_curricular
+
+-- This is for credits only
+-- department,program_core,humanties_core,science_core,general_engineering,capstone,internship,extra_curricular
+-- program_elective,humanities_elective,science_elective,open_elective
